d564682da106260efed8b68dac60f5012105e393
Vorname Nachname <>
2022-12-13 13:12:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#def pz(zahldeseingangs):
+#  primzohl = True
+#  zahldeslaufens = 2
+#  print(zahldeslaufens)
+#  while zahldeslaufens < zahldeseingangs:
+#    if zahldeseingangs % zahldeslaufens == 0:
+#      primzohl = False
+      
+while True:
+  jesus = input("bitte")
+  print(jesus)
+  if 1 == 1:
+    break
+
+try:
+  jj = float(jesus) + 1
+except:
+  print("error")
